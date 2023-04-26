43018bb9a3e2f07f2452c2d8ca3be4c5f220abe2
Kate <kate.smith@digital.justice.gov.uk>
2023-04-26 12:47:42 +0100

    Has a calculator test and works locally

---
+++ README.md
@@ -0,0 +1,10 @@
+To download this repo:
+1. Clone it `git clone {repo ssh}`
+2. Download the dependencies `npm install`
+
+To run the tests:
+`npm test` 
+(This works because I set up a script in the package.json to run a script, if you add any more tests it will need to be amended)
+
+NB:
+If there are any files you don't want added to github put them in the `.gitignore` file
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require './src/Calculator.php';
+final class CalculatorTest extends TestCase
+{
+    private $calculator;
+
+    protected function setUp(): void
+    {
+        $this->calculator = new Calculator();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->calculator = NULL;
+    }
+
+    public function testAdd()
+    {
+        $result = $this->calculator->add(1, 2);
+        $this->assertEquals(3, $result);
+    }
+
+}
